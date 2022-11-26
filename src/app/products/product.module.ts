d87e31647ef95c345c7516582b33e8c73d6b448d
dkvitus <dkvitus@gmail.com>
2022-11-26 11:19:06 -0500

    tabs added

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -8,6 +8,7 @@ import { SharedModule } from '../shared/shared.module';
 import { MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTabsModule} from '@angular/material/tabs'
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import {MatIconModule} from '@angular/material/icon';
   ],
   imports: [
     MatButtonModule, 
+    MatTabsModule,
     MatButtonToggleModule,
     MatIconModule,
     RouterModule.forChild([
